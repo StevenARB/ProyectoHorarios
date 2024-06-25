8c2c1410248d315dca03565fbc2690bf82aa1145
StevenRB <retanasteven8@gmail.com>
2024-06-24 22:30:53 -0600

    Add project files.

---
+++ SC-701_ProyectoG4_Horarios/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using SC_701_ProyectoG4_Horarios.Models;
+using System.Diagnostics;
+
+namespace SC_701_ProyectoG4_Horarios.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SC-701_ProyectoG4_Horarios/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SC_701_ProyectoG4_Horarios.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
